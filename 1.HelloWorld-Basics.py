3393ef905dd8e4406a121b843a16f672559d4144
Akshat Nehra <akshatnehra1999@gmail.com>
2021-01-19 20:49:41 +0530

    Hello World and basics of PYTHON

---
+++ 1.HelloWorld-Basics.py
@@ -0,0 +1,22 @@
+# Hello World program in PYTHON
+print("Hello World!")
+
+"""
+ Python code in INTERPRETED
+
+ unlike COMPILER (which translates whole program in machine language at once),
+ interpreter translates the just one statement at a time (in machine language).
+
+ overall time taken by compiler to translate program in machine language is less
+ as compared to interpreter
+ COMPILER is faster than INTERPRETER
+
+ interpreter does not generate an intermediary code and hence requires less memory
+ compiler always generates an intermediary object code hence more memory is needed
+
+"""
+
+# A sequence of operands and operators is called is called an EXPRESSION
+print("Python" * 10)
+
+# Python will be printed 10 times, in python first expression is evaluated then print statement executes
