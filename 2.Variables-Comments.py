7934a01f50cba84a94ca7a42457df354e61110b8
Akshat Nehra <akshatnehra1999@gmail.com>
2021-01-19 21:15:40 +0530

    Variables and Comments

---
+++ 2.Variables-Comments.py
@@ -0,0 +1,35 @@
+
+# Variables in PYTHON
+
+age = 51    # integer variable (stores INTEGERS)
+
+weight = 88.56  # float variable (stores DECIMAL values)
+
+name = "Akshat Nehra"   # string variable (stores a sequence of CHARACTERS)
+
+bool = True    # boolean variabe (stores BOOLEAN value, either True or False)
+
+
+
+
+# Comments in PYTHON
+"""
+    Comments in PYTHON are used to
+    1. Explain the CODE
+    2. Make our code more readable
+    3. Intepreter ignore comments and do not execute them 
+"""
+
+
+# Single Line comments can be written by using "#" symbol, scope till END OF LINE
+
+
+"""
+It is a MULTI LINE COMMENT
+It is written between 3 consecutive DOUBLE QUOTES
+"""
+
+'''
+This is another way to write multi line comments
+using 3 consecutive SINGLE QUOTES
+'''
